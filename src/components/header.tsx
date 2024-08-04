562af5c08f0f5de02a51fbb3d3dbb16ab0fb52dc
LrxGregory <lrxgregory@gmail.com>
2024-08-04 16:17:02 +0200

    add some style header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,6 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ selectedMap, onMapChange, playerNumber, onPlayerNumberChange }) => {
     const maps = ['USA', 'Europe'];
 
-
     const handleMapChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const selectedMap = event.target.value;
         onMapChange(selectedMap);
@@ -24,21 +23,22 @@ const Header: React.FC<HeaderProps> = ({ selectedMap, onMapChange, playerNumber,
     };
 
     return (
-        <div className="w-full bg-blue-500 p-4">
-            <h1 className="text-white text-center text-2xl font-bold mb-4">Ticket To Ride Score</h1>
-            <div className="max-w-4xl mx-auto p-4">
-                <div className="flex space-x-4">
+        <div className="w-full bg-slate-600	p-6">
+            <h1 className="text-white text-center text-3xl font-extrabold mb-6">Ticket To Ride Score</h1>
+            <div className="max-w-4xl mx-auto px-4">
+                <div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-6">
                     {/* Sélecteur de carte */}
                     <div className="flex-1 min-w-0">
-                        <div className="flex items-center space-x-2">
-                            <label htmlFor="map" className="text-sm font-medium text-gray-700 w-36">
+                        <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2">
+                            <label htmlFor="map" className="text-sm font-medium text-gray-100 w-full sm:w-auto">
                                 Maps:
                             </label>
                             <select
                                 id="map"
                                 value={selectedMap}
                                 onChange={handleMapChange}
-                                className="block flex-1 border border-gray-300 bg-white text-gray-900 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                className="block w-full sm:w-64 border border-gray-300 bg-gray-50 text-gray-900 rounded-lg shadow-md focus:ring-brown-500 focus:border-brown-500 sm:text-md transition-all duration-300 ease-in-out hover:border-brown-400 text-center"
+                                style={{ textAlignLast: 'center' }} // Centrer le texte du select
                             >
                                 {maps.map((mapOption) => (
                                     <option key={mapOption} value={mapOption}>
@@ -49,8 +49,8 @@ const Header: React.FC<HeaderProps> = ({ selectedMap, onMapChange, playerNumber,
                         </div>
                     </div>
                     <div className="flex-1 min-w-0">
-                        <div className="flex items-center space-x-2">
-                            <label htmlFor="playersNumber" className="text-sm font-medium text-gray-700 w-52">
+                        <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2">
+                            <label htmlFor="playersNumber" className="text-sm font-medium text-gray-100 w-full sm:w-auto">
                                 Number of players (2-5):
                             </label>
                             <input
@@ -61,7 +61,7 @@ const Header: React.FC<HeaderProps> = ({ selectedMap, onMapChange, playerNumber,
                                 max="5"
                                 value={playerNumber}
                                 onChange={handlePlayerNumberChange}
-                                className="block flex-1 border border-gray-300 bg-white text-gray-900 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                className="block w-full sm:w-64 border border-gray-300 bg-gray-50 text-gray-900 rounded-lg shadow-md focus:ring-brown-500 focus:border-brown-500 sm:text-md transition-all duration-300 ease-in-out hover:border-brown-400 text-center"
                             />
                         </div>
                     </div>
